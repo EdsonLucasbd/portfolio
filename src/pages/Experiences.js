dde09b2a5d2eabb0e970ab8acb9763192e628d4c
EdsonLucasbd <edsonlucas.lucas@gmail.com>
2021-04-28 06:57:38 -0300

    animations

--- src/pages/Experiences.js
+++ src/pages/Experiences.js
@@ -1,16 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import '../styles/pages/Experiences.css';
 
 export default function Experiences() {
+
+  useEffect(() => {
+    const target = document.querySelectorAll('[data-anime]');
+    const animationClass = 'animate';
+
+    const animeScroll = () => {
+      const windowTop = window.pageYOffset + (window.innerHeight * 0.75);
+      target.forEach(function(element) {
+        if (windowTop > element.offsetTop) {
+          element.classList.add(animationClass)
+        }
+        else element.classList.remove(animationClass);
+      })
+    }
+
+    if(target.length) {
+      window.addEventListener('scroll', animeScroll)
+      }
+  },[]);
   
   return (
     <section id='experiences'>
       <div className='expContainer'>
         <h5>&lt;experiences&gt;</h5>
 
-        <div className='line'/>
+        <div className='line' data-anime='up'/>
 
-        <div className="firstExpContainer">
+        <div className="firstExpContainer" data-anime='right'>
           <div className='circle'/>
           <p id='year'>2020-2021</p>
           <p id='expInfo'>
@@ -19,7 +38,7 @@ export default function Experiences() {
           </p>
         </div>
         
-        <div className="secondExpContainer">
+        <div className="secondExpContainer" data-anime='left'>
           <div className='circle2'/>
           <p id='year'>2021-Atualmente</p>
           <p id='expInfo'>
@@ -33,10 +52,10 @@ export default function Experiences() {
 
       <div className='tecnologiesContainer'>
         <h5>&lt;tecnologies&gt;</h5>
-        <p id='js'>JS</p>
-        <p id='react'>React</p>
-        <p id='reactNative'>React Native</p>
-        <p id='python'>Python</p>
+        <p id='js'data-anime='left'>JS</p>
+        <p id='react'data-anime='right'>React</p>
+        <p id='reactNative'data-anime='left'>React Native</p>
+        <p id='python'data-anime='right'>Python</p>
         <h5>&lt;/tecnologies&gt;</h5>
       </div>
     </section>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,44 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import '../styles/pages/Home.css';
 
 export default function Home() {
+  useEffect(() => {
+    const target = document.querySelectorAll('[data-anime]');
+    const animationClass = 'animate';
+    const section = document.querySelector('#home');
+
+    const animation = () => {
+      const windowTop = window.pageYOffset + (window.innerHeight * 0.75);
+      target.forEach(function(element) {
+        if (windowTop > element.offsetTop) {
+          element.classList.add(animationClass);
+        }
+        else element.classList.remove(animationClass);
+      })
+    }
+
+    animation();
+
+    if(target.length) {
+      section.addEventListener('load', animation);
+    }
+  },[]);
+
   return (
     <section id='home'>
       <div className="nameContainer">
         <h5>&lt;developer&gt;</h5>
-        <p>Edson<br/>Lucas</p>
+        <p data-anime='left'>Edson<br/>Lucas</p>
         <h5>&lt;/developer&gt;</h5>
       </div>
       
-      <img src="/fotoPortfolio.png" alt="profile avatar"/>
+      <img data-anime='down' src="/fotoPortfolio.png"  alt="profile avatar"/>
 
       <div className="aboutContainer">
         <h5>&lt;about_me&gt;</h5>
-        <p>Aspirante a programador full stack</p>
-        <p>Gamer</p>
-        <p>Amante de séries</p>
+        <p data-anime='left'>Aspirante a programador full stack</p>
+        <p data-anime='right'>Gamer</p>
+        <p data-anime='left'>Amante de séries</p>
         <h5>&lt;/about_me&gt;</h5>
       </div>
     </section>
