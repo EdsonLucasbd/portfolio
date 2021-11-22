901edd4735f9dfcb83d9446059a412947a8b3ef6
Edson Lucas <front1.shopack@gmail.com>
2021-11-22 08:21:57 -0300

    scroll icon

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import '../styles/pages/Home.css';
 import '../styles/static/blobz.css'
+import '../styles/static/scrollIcon.css'
 
 export default function Home() {
 
@@ -43,18 +44,16 @@ export default function Home() {
           <path d="M410.6 78.8c36 52.5 36.1 126 19.2 194C412.9 340.7 379 403 330 421.9c-49 19-113.1-5.3-178.6-34C85.8 359.2 18.7 326.1 3.5 276.4-11.7 226.7 25 160.3 71.7 105.3 118.3 50.3 174.8 6.8 239 .7c64.1-6 135.7 25.5 171.6 78.1z"></path>
         </svg>
       </div>
+      <div className="scrollIcon"></div>
     </div>
 
-      {/* <img src="/smoke1.png" alt="smoke" className='smoke'/> */}
-
-      <div className="aboutContainer">
-        <h5>&lt;about_me&gt;</h5>
-        <p data-anime='left'>Estudante de sistemas <br/>de informação na Universidade <br/>do estado da Bahia.</p>
-        <p data-anime='right'>Programador front-end <br/>e mobile.</p>
-        <p data-anime='left'>Gamer.</p>
-        <p data-anime='right'>Amante de séries.</p>
-        <h5>&lt;/about_me&gt;</h5>
-      </div>
+    <div className="aboutContainer">
+      <h5>&lt;about_me&gt;</h5>
+      <p data-anime='right'>Programador front-end e mobile.</p>
+      <p data-anime='left'>Gamer e amante de séries.</p>
+      {/* <p data-anime='right'>Amante de séries.</p> */}
+      <h5>&lt;/about_me&gt;</h5>
+    </div>
     </div>
   )
 }
